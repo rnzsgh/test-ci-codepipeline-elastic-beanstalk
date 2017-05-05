78a8d7b541e4a3a6ebff738afa651aa4c6277280
Ryan <ryannitz@amazon.com>
2017-05-05 11:55:20 -0400

    Allow all hosts

--- app/app/settings.py
+++ app/app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wmstmtqy^jzw%tih(@(85mop%=ku+lilyv!)or^u%&&#%(+yb3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' *' *' *' *' *' *' *' ]
 
 
 # Application definition
